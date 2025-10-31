6059cd664dd99e81a984c1f23812a5b6b148d778
j-devel <38098238+j-devel@users.noreply.github.com>
2025-10-31 11:30:55 +0700

    WIP Mods for mnist cases

--- src/transduction/wsdan/main.py
+++ src/transduction/wsdan/main.py
@@ -11,6 +11,8 @@ logger.setLevel(level=logging.DEBUG if 1 else logging.INFO)
 
 def main():
 
+    CLASS_NAMES_SORTED_THYROID = ['benign', 'malignant']
+
     if 0:  # adaptation of 'compare.{ipynb,py}' exported from https://colab.research.google.com/drive/1kxMFgo1LyVqPYqhS6_UJKUsVvA2-l9wk
         from .demo import doppler_compare
         doppler_compare()
@@ -21,7 +23,7 @@ def main():
 
         from .demo import TEST_DS_PATH_DEFAULT
         ckpt = 'densenet_224_8_lr-1e5_n4_95.968.ckpt'  # 0.9xx, LGTM
-        demo_test(ckpt, 'densenet121', TEST_DS_PATH_DEFAULT, 224, 8)
+        demo_test(ckpt, CLASS_NAMES_SORTED_THYROID, 'densenet121', TEST_DS_PATH_DEFAULT, 224, 8)
 
     if 0:
         total_epochs = 2
@@ -59,7 +61,7 @@ def main():
             'malignant': ['Markers_Train_Remove_Markers/Malignant_Remove/test'],
         }, root=dataset_doppler)  # 75 75
 
-        demo_test(ckpt, model, test_ds_path)
+        demo_test(ckpt, CLASS_NAMES_SORTED_THYROID, model, test_ds_path)
 
     if 0:  # experiment - 'heatmap-compare-doppler_100c-TrueFalse--rounds--tests_100d'
         # ckpt = './heatmap-compare-doppler_100c-TrueFalse--rounds/train-test-doppler-False/output/demo_train/resnet34_250_8_lr-1e5_n4'
@@ -69,7 +71,7 @@ def main():
         ckpt = './heatmap-compare-doppler_100c-TrueFalse--rounds/train-test-doppler-True/run-2/output/demo_train_with_doppler/resnet34_250_8_lr-1e5_n4'
         # ckpt = './heatmap-compare-doppler_100c-TrueFalse--rounds/train-test-doppler-True/run-3/output/demo_train_with_doppler/resnet34_250_8_lr-1e5_n4'
 
-        demo_test(ckpt, 'resnet34', build_dataset({
+        demo_test(ckpt, CLASS_NAMES_SORTED_THYROID, 'resnet34', build_dataset({
             'benign': ['Markers_Train_Remove_Markers/Benign_Remove/test'],
             'malignant': ['Markers_Train_Remove_Markers/Malignant_Remove/test'],
         }, root='Dataset_doppler_100d'))
@@ -81,14 +83,14 @@ def main():
             'malignant': ['Markers_Train_Remove_Markers/Malignant_Remove/test'],
         }, root='Dataset_doppler_100d')
 
-        demo_test(ckpt, 'resnet34', test_ds_path, 250, 8)
+        demo_test(ckpt, CLASS_NAMES_SORTED_THYROID, 'resnet34', test_ds_path, 250, 8)
 
     if 0:  # experiment - default
         total_epochs = 10
         model = 'resnet34'
 
         ckpt = demo_train(total_epochs, model)
-        demo_test(ckpt, model)
+        demo_test(ckpt, CLASS_NAMES_SORTED_THYROID, model)
 
     if 0:  # !!!! k-fold dev
         #total_epochs = 1
@@ -111,7 +113,7 @@ def main():
             ],
         }
         ckpt = demo_train(total_epochs, model, ds_paths)
-        demo_test(ckpt, model)
+        demo_test(ckpt, CLASS_NAMES_SORTED_THYROID, model)
 
     if 0:  # siriraj_original_Testset_26.zip
         test_ds_path = build_dataset({
@@ -119,18 +121,19 @@ def main():
             'malignant': ['test26/Malignant'],
         }, root='siriraj_original_Testset_26')
 
-        ##demo_test('xxxx/ckpt', 'resnet34', test_ds_path)
-        demo_test('densenet121_250_8_lr-1e5_n4', 'densenet121', test_ds_path)
+        ##demo_test('xxxx/ckpt', CLASS_NAMES_SORTED_THYROID, 'resnet34', test_ds_path)
+        demo_test('densenet121_250_8_lr-1e5_n4', CLASS_NAMES_SORTED_THYROID, 'densenet121', test_ds_path)
 
     if 1:  # non-binary classification
         total_epochs = 2
         model = 'resnet34'
 
         #====
-        #ds_paths, class_names_sorted = get_thyroid_ds_paths('100g', debug=False)
+        ds_paths, class_names_sorted = get_thyroid_ds_paths('100g', debug=False)
+        print(class_names_sorted)
         #ckpt = demo_train(total_epochs, model, ds_paths)
         #====
-        if 0:
+        if 1:
             ds_paths, class_names_sorted = get_mnist_ds_paths(  # mnist 10-class
                 root_train='datasets_vit/pngs/train--sparse',  # 128 samples
                 root_test='datasets_vit/pngs/test--sparse')  # 10 samples
@@ -150,9 +153,9 @@ def main():
             #ckpt = 'output--colab-wsdan-mnist/eps2/resnet34_250_8_lr-1e5_n4'  # eps=2, test 4/10 poor
             ckpt = 'output--colab-wsdan-mnist/eps8/resnet34_250_8_lr-1e5_n4'  # eps=8, test 7/10
 
-            results = demo_test(ckpt, model, ds_path_test)
+            demo_test(ckpt, class_names_sorted, model, ds_path_test)
         #====
-        if 1:
+        if 0:
             ds_paths, class_names_sorted = get_mri_ds_paths(
                 #'erica', root='datasets_mri/50-001')  # colab --> 1200
                 'erica', root='datasets_mri/50-001-100')  # debug --> 202
